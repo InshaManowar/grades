1a0d49581a111aae9b6456cec056b29b76ea3d6f
inshamanowar <inshamanowar22@gmail.com>
2021-06-25 23:40:59 +0530

    bug

--- grade/settings.py
+++ grade/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*q0e#xei&fe-rd)98lyjo20l_j3jtm^bp*=1d+dk1xmx5w=tl2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['forlazyppl.pythonanywhere.com','127.0.0.1']
 
